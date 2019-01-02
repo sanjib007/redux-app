92e06bee4e3c18931d15879b7309c0d9bae355f0
R041411012 <loton1984@gmail.com>
2019-01-02 19:55:31 +0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+import { createStore } from 'redux';
+
+function reducer(state, action) {
+    console.log(action);
+    if(action.type === "changeState"){
+        return action.payload.newState;
+    }
+    return 'State';
+}
+
+const store = createStore(reducer);
+
+console.log(store.getState());
+
+const action = {
+    type: 'changeState',
+    payload : {
+        newState: 'New State'
+    }
+}
+
+store.dispatch(action);
+
+console.log('new',store.getState());
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
